8110a7aa4a0f7012e5746129d96efc4d64036f00
Agost Biro <agostbiro@gmail.com>
2020-11-24 20:52:23 +0100

    Add comments

--- src/hello_clojure/core.clj
+++ src/hello_clojure/core.clj
@@ -1,18 +1,24 @@
+;; Define namespace and import packages
 (ns hello-clojure.core
   (:gen-class)
   (:require
     [clojure.data.json :as json]
     [clj-http.client :as client]))
 
+;; Get the Github API url from the user name
 (defn github-api-for-user [user-name]
   (str "https://api.github.com/users/" user-name))
 
 (defn -main
   "Query Github API for a user's real name"
   [& args]
+  ;; Get the user name from command line arguments
   (let [user-name (first args)
+        ;; Query the github API and get the response body as JSON string
         body (:body (client/get (github-api-for-user user-name)))
+        ;; Parse JSON string to map
         result (json/read-str body)
+        ;; Get the name member from the map
         real-name (get result "name")]
     (if (nil? real-name)
       (println "This Github user has not made their name public")
